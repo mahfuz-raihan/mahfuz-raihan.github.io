7c50591957f4f01716b31e6e16a65c8860cfd409
mahfuz-raihan <mahfuz_raihan@outlook.com>
2025-07-23 05:28:19 +0600

    Add deployment scripts and configure Vite for GitHub Pages
    - Added "predeploy" and "deploy" scripts to package.json for building and deploying the app to GitHub Pages.
    - Installed "gh-pages" package as a dependency.
    - Set the base path in vite.config.js for proper routing on GitHub Pages.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/mahfuz-raihan.github.io/', // Replace REPO_NAME with your GitHub repo name
   plugins: [
     react(),
   ],
