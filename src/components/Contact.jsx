2761a060299b8c85553040706c2fe52db9238e3c
mahfuz-raihan <mahfuz_raihan@outlook.com>
2025-08-03 01:29:59 +0600

    fix the white screen issue whole dark mode.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -60,11 +60,11 @@ export default function Contact() {
                   <motion.a
                     key={index}
                     href="#"
-                    className="bg-white p-2 lg:p-3 rounded border-2 border-black"
-                    whileHover={{ scale: 1.1, backgroundColor: "#000", color: "#fff" }}
+                    className="bg-white dark:bg-gray-800 p-2 lg:p-3 rounded border-2 border-black dark:border-white"
+                    whileHover={{ scale: 1.1 }}
                     whileTap={{ scale: 0.9 }}
                   >
-                    <Icon className="w-4 h-4 lg:w-5 lg:h-5" />
+                    <Icon className="w-4 h-4 lg:w-5 lg:h-5 text-black dark:text-white" />
                   </motion.a>
                 ))}
               </div>
